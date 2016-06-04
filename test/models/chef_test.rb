5dca704306bc8547b8a810783b7c747025b46a54
Carlos Matsumoto <ckenlli@gmail.com>
2016-06-04 02:15:51 +0000

    create test for the models, change recipe table to add chef_id and validations to the models

---
+++ test/models/chef_test.rb
@@ -0,0 +1,64 @@
+require 'test_helper'
+
+class ChefTest < ActiveSupport::TestCase
+  
+  def setup
+    @chef = Chef.new(chefname: "john", email: "john@test.com")
+  end
+  
+  test "chef should be valid" do
+    assert @chef.valid?
+  end
+  
+  test "chef should be present" do
+    @chef.chefname = ""
+    assert_not @chef.valid?
+  end
+  
+  test "chefname should be not too long" do 
+    @chef.chefname = "a" * 41
+    assert_not @chef.valid?
+  end
+  
+  test "chefname should be not too short" do
+    @chef.chefname = "aa"
+    assert_not @chef.valid?
+  end
+  
+  test "email should be present" do
+    @chef.email = ""
+    assert_not @chef.valid?
+  end
+  
+  test "email should not be out of bounds" do
+    @chef.email = "a" * 101 + '@test.com'
+    assert_not @chef.valid?
+  end
+  
+  test "email should be unique" do
+    dup_chef = @chef.dup
+    dup_chef.email = @chef.email.upcase
+    @chef.save
+    assert_not dup_chef.valid?
+  end
+  
+  test "email should accept valid addresses" do
+    valid_addresses = %w[user@eee.com R_TDD-DS@eee.hello.org user@example.com
+                        first.last@eem.au laura+joe@monk.com]
+    
+    valid_addresses.each do |va|
+      @chef.email = va 
+      assert @chef.valid?, '#{va.inspect} should be valid'
+    end
+  end
+  
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@eee,com R_TDD-DSeee.hello.org user@example.
+                          first.last@_eem_.au laura.joe@mo+nk.com]
+                          
+    invalid_addresses.each do |ia|
+      @chef.email = ia 
+      assert_not @chef.valid?, '#{ia.inspect} should be invalid'
+    end
+  end
+end
